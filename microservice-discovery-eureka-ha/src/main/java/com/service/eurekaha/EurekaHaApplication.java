c369b60f60f62cc47fe0cb5d85342bee24c1934d
Garychi <mos20229@livemail.tw>
2019-05-14 21:46:39 +0800

    add peer
    add peer

--- microservice-discovery-eureka-ha/src/main/java/com/service/eurekaha/EurekaHaApplication.java
+++ microservice-discovery-eureka-ha/src/main/java/com/service/eurekaha/EurekaHaApplication.java
@@ -1,4 +1,4 @@
-package com.service.microservicediscoveryeureka;
+package com.service.eurekaha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaHaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaHaApplication.class, args);
     }
 
 }
--- microservice-discovery-eureka-ha/src/test/java/com/service/eurekaha/EurekaHaApplicationTests.java
+++ microservice-discovery-eureka-ha/src/test/java/com/service/eurekaha/EurekaHaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.service.microservicediscoveryeureka;
+package com.service.eurekaha;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaApplicationTests {
+public class EurekaHaApplicationTests {
 
     @Test
     public void contextLoads() {
